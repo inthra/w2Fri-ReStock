3765f448d2146796a716da216800740f14ca798b
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-01 14:07:38 -0700

    Modify Main, Register, and UserArea activity to almost link to each other properly.

--- app/src/main/java/com/epicodus/sportstats/MainActivity.java
+++ app/src/main/java/com/epicodus/sportstats/MainActivity.java
@@ -1,7 +1,12 @@
 package com.epicodus.sportstats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText mUserName = (EditText) findViewById(R.id.etUserName);
+        final EditText mPassWord = (EditText) findViewById(R.id.etPassWord);
+
+        final Button mSignIn = (Button) findViewById(R.id.bSignIn);
+
+        final TextView mRegisterLink = (TextView) findViewById(R.id.tvRegisterHere);
+        mRegisterLink.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+               startActivity(intent);
+           }
+        });
     }
 }
--- app/src/main/java/com/epicodus/sportstats/RegisterActivity.java
+++ app/src/main/java/com/epicodus/sportstats/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.epicodus.sportstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText mFirstName = (EditText) findViewById(R.id.etFirstName);
+        final EditText mLastName = (EditText) findViewById(R.id.etLastName);
+        final EditText mEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText mUserName = (EditText) findViewById(R.id.etUserName);
+        final EditText mPassWord = (EditText) findViewById(R.id.etPassWord);
+
+        final Button mRegisterButton = (Button) findViewById(R.id.bRegister);
+
     }
 }
