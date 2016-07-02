9a5d4b6c236d92d0d106374f7fdc7357daee89a2
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-02 13:30:48 -0700

    Add Toast in the RegisterActivity to display text (Register Success) on the screen.

--- app/src/main/java/com/epicodus/sportstats/MainActivity.java
+++ app/src/main/java/com/epicodus/sportstats/MainActivity.java
@@ -8,22 +8,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+    private Button mSignInButton;
+    private TextView mRegisterLink;
+    private EditText mUserName;
+    private EditText mPassWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText mUserName = (EditText) findViewById(R.id.etUserName);
-        final EditText mPassWord = (EditText) findViewById(R.id.etPassWord);
+        mUserName = (EditText) findViewById(R.id.etUserName);
+        mPassWord = (EditText) findViewById(R.id.etPassWord);
 
-        final Button mSignIn = (Button) findViewById(R.id.bSignIn);
+        mSignInButton = (Button) findViewById(R.id.bSignIn);
 
-        final TextView mRegisterLink = (TextView) findViewById(R.id.tvRegisterHere);
+        mRegisterLink = (TextView) findViewById(R.id.tvRegisterHere);
         mRegisterLink.setOnClickListener(new View.OnClickListener() {
            @Override
-           public void onClick(View v) {
+           public void onClick(View view) {
                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                startActivity(intent);
            }
--- app/src/main/java/com/epicodus/sportstats/RegisterActivity.java
+++ app/src/main/java/com/epicodus/sportstats/RegisterActivity.java
@@ -2,23 +2,38 @@ package com.epicodus.sportstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.app.ProgressDialog.show;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Button mRegisterButton;
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mEmail;
+    private EditText mUserName;
+    private EditText mPassWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText mFirstName = (EditText) findViewById(R.id.etFirstName);
-        final EditText mLastName = (EditText) findViewById(R.id.etLastName);
-        final EditText mEmail = (EditText) findViewById(R.id.etEmail);
-        final EditText mUserName = (EditText) findViewById(R.id.etUserName);
-        final EditText mPassWord = (EditText) findViewById(R.id.etPassWord);
+        mFirstName = (EditText) findViewById(R.id.etFirstName);
+        mLastName = (EditText) findViewById(R.id.etLastName);
+        mEmail = (EditText) findViewById(R.id.etEmail);
+        mUserName = (EditText) findViewById(R.id.etUserName);
+        mPassWord = (EditText) findViewById(R.id.etPassWord);
 
-        final Button mRegisterButton = (Button) findViewById(R.id.bRegister);
-
+        mRegisterButton = (Button) findViewById(R.id.bRegister);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
