f67ca8bed999b63af7ef5b5daaf067551144fc24
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-02 16:33:35 -0700

    Add TeamsActivity to display teams with scores.

--- app/src/main/java/com/epicodus/sportstats/MainActivity.java
+++ app/src/main/java/com/epicodus/sportstats/MainActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.sportstats;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,10 +12,11 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mSignInButton;
-    private TextView mRegisterLink;
+    public static final String TAG = MainActivity.class.getSimpleName();
     private EditText mUserName;
     private EditText mPassWord;
+    private Button mSignInButton;
+    private TextView mRegisterLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,24 @@ public class MainActivity extends AppCompatActivity {
         mPassWord = (EditText) findViewById(R.id.etPassWord);
 
         mSignInButton = (Button) findViewById(R.id.bSignIn);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String usernameInput = mUserName.getText().toString();
+//                Log.d(TAG, usernameInput);
+                Intent intent = new Intent(MainActivity.this, TeamsActivity.class);
+//                intent.putExtra("username", usernameInput);
+                startActivity(intent);
+            }
+        });
 
         mRegisterLink = (TextView) findViewById(R.id.tvRegisterHere);
         mRegisterLink.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
+            @Override
+            public void onClick(View v) {
                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                startActivity(intent);
-           }
+            }
         });
     }
 }
---
+++ app/src/main/java/com/epicodus/sportstats/TeamsActivity.java
@@ -0,0 +1,37 @@
+package com.epicodus.sportstats;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class TeamsActivity extends AppCompatActivity {
+    private ListView mListTeams;
+    private ListView mListScores;
+    private TextView mUsername;
+    private String[] teams = new String[] {"Switzerland vs Poland", "Wales vs N.Ireland", "Croatia vs Portugal", "France vs Ireland", "Germany vs Slovakia", "Hungry vs Belgium", "Italy vs Spain", "England vs Iceland", "Poland vs Portugal", "Wales vs Belgium"};
+    private String[] scores = new String[] {"1 - 1", "1 - 0", "0 - 1", "2 - 1", "3 - 0", "0 - 4", "2 - 0", "1 - 2", "1 - 1", "3 - 1"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams);
+
+//        mUsername = (TextView) findViewById(R.id.etUsername);
+//        Intent intent = getIntent();
+//        String username = intent.getStringExtra("username");
+//        mUsername.setText("Here is the text: " + username);
+
+        mListTeams = (ListView) findViewById(R.id.lvTeams);
+        mListScores = (ListView) findViewById(R.id.lvScores);
+
+        ArrayAdapter adapterTeams = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, teams);
+        ArrayAdapter adapterScores = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, scores);
+
+        mListTeams.setAdapter(adapterTeams);
+        mListScores.setAdapter(adapterScores);
+
+    }
+}
