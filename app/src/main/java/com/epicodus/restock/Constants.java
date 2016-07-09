8827f3750a006fc941e841b6270a143bcae59edb
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-09 11:40:03 -0700

    Create Constants class, Foodcart class, and YelpService class.

---
+++ app/src/main/java/com/epicodus/restock/Constants.java
@@ -0,0 +1,10 @@
+package com.epicodus.restock;
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food+cart";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
--- app/src/main/java/com/epicodus/restock/ui/DeliveryActivity.java
+++ app/src/main/java/com/epicodus/restock/ui/DeliveryActivity.java
@@ -3,21 +3,52 @@ package com.epicodus.restock.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.epicodus.restock.R;
+import com.epicodus.restock.services.YelpService;
+
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class DeliveryActivity extends AppCompatActivity {
+    public static final String TAG = DeliveryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
 
+//        getRestaurants(location);
+
     }
+
+//    private void getRestaurants(String location) {
+//        final YelpService yelpService = new YelpService();
+//        yelpService.findRestaurants(location, new Callback() {
+//
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                try {
+//                    String jsonData = response.body().string();
+//                    Log.v(TAG, jsonData);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
 }
