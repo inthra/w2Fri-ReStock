2e5887ece5636e24ffafa0a639096d65590da33c
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-15 18:21:14 -0700

    Add log in fucntionality that works properly.

--- app/src/main/java/com/epicodus/restock/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/restock/ui/MainActivity.java
@@ -1,28 +1,37 @@
 package com.epicodus.restock.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.restock.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.tvHeader) TextView mAppNameHeader;
-    @Bind(R.id.etUsername) EditText mUsername;
+    @Bind(R.id.etEmail) EditText mEmail;
     @Bind(R.id.etPassword) EditText mPassword;
     @Bind(R.id.bSignIn) Button mSignInButton;
     @Bind(R.id.tvRegisterHere) TextView mRegisterHere;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +44,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
 
+        createAuthProgressDialog();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(MainActivity.this, RolesActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
         mSignInButton.setOnClickListener(this);
         mRegisterHere.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
@@ -52,9 +90,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
     private void loginWithPassword() {
-        String usernameInput = mUsername.getText().toString();
-        Intent intent = new Intent(MainActivity.this, RolesActivity.class);
-        startActivity(intent);
+        String emailInput = mEmail.getText().toString().trim();
+        String passwordInput = mPassword.getText().toString().trim();
+        if (emailInput.equals("")) {
+            mEmail.setError("Please enter your email");
+            return;
+        }
+        if (passwordInput.equals("")) {
+            mPassword.setError("Please enter your password");
+            return;
+        }
+
+        mAuthProgressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mAuthProgressDialog.dismiss();
+
+                if (!task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
