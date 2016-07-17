0e1945c1e5ed375d40e3a50c35f53d58075055aa
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-07-16 17:00:15 -0700

    Add onClick to the OwnerActivity.

--- app/src/main/java/com/epicodus/restock/ui/OwnerActivity.java
+++ app/src/main/java/com/epicodus/restock/ui/OwnerActivity.java
@@ -1,15 +1,33 @@
 package com.epicodus.restock.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.epicodus.restock.R;
 
-public class OwnerActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class OwnerActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.addFoodtrucksButton) Button mAddFoodtrucksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+        ButterKnife.bind(this);
+
+        mAddFoodtrucksButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mAddFoodtrucksButton) {
+            Intent intent = new Intent(OwnerActivity.this, DeliveryListActivity.class);
+            startActivity(intent);
+        }
     }
 }
